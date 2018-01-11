051a8912a6f35ec558573bc083cafc3466f6bbe4
Ortich <d.ortich.oficial@gmail.com>
2018-01-11 19:46:14 +0100

    Añadida mi clase

--- src/ejerciciogrupo/ClaseA.java
+++ src/ejerciciogrupo/ClaseA.java
@@ -7,15 +7,8 @@ package ejerciciogrupo;
 
 /**
  *
- * @author Alex
+ * @author Daniel Ortiz Vallejo
  */
-public class EjercicioGrupo {
-    
-    /**
-     * @param args the command line arguments
-     */
-    public static void main(String[] args) {
-	// TODO code application logic here
-    }
+public class ClaseA {
     
 }
